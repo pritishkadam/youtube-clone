368168368ed485837dd3ff44130c7185c269216c
Pritish Kadam <prtshkdm18@gmail.com>
2023-08-28 21:12:37 +0530

    update search suggestion API URL

--- src/util/constants.js
+++ src/util/constants.js
@@ -7,7 +7,7 @@ export const YOUTUBE_URL = `https://youtube.googleapis.com/youtube/v3/videos?par
 export const YOUTUBE_VIDEOS_URL = `https://www.googleapis.com/youtube/v3/videos?part=id,snippet,contentDetails,statistics,player&regionCode=IN&maxResults=50&key=${GOOGLE_API_KEY}`;
 
 export const YOUTUBE_SEARCH_SUGGESTION_API =
-  'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
+  'https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
 
 export const YOUTUBE_SEARCH_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&key=${GOOGLE_API_KEY}&q=`;
 
